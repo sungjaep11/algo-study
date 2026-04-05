cf366b21bdd016a14db5f88e0a363747c726d73d
배서연 <alicebsy@naver.com>
2026-04-05 23:09:51 +0900

    Boj/silver/1100 (#671)
    * [Bronze II] Title: 8진수 2진수, Time: 292 ms, Memory: 22184 KB -BaekjoonHub
    
    * auto: merge solved problems into alicebsy folder
    
    * [Bronze II] Title: 하얀 칸, Time: 104 ms, Memory: 14152 KB -BaekjoonHub
    
    * auto: merge solved problems into alicebsy folder
    
    * [Bronze II] Title: 하얀 칸, Time: 104 ms, Memory: 14184 KB -BaekjoonHub
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ alicebsy/백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        
+        for (int i = 0; i < 8; i++) {
+            String line = br.readLine();
+            
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0 && line.charAt(j) == 'F') {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
